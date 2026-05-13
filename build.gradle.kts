0a990a05a4c66cc0b73302d3a9a3122e7b179c48
bjornko <bjorn.vaggen.konestabo@nav.no>
2026-05-13 15:09:23 +0200

    Bump bcpkix-jdk18on til 1.84 pga. sårbarhet

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,6 +107,10 @@ configurations.all {
                 useVersion("1.84")
                 because("Bouncy Castle Has Covert Timing Channel Vulnerability. Affected version >= 1.71, < 1.84")
             }
+            if (requested.group == "org.bouncycastle" && requested.name == "bcpkix-jdk18on") {
+                useVersion("1.84")
+                because("CVE-2026-5588: Use of a Broken or Risky Cryptographic Algorithm vulnerability in Legion of the Bouncy Castle Inc. BC-JAVA bcpkix on all (pkix modules). >= 1.49, < 1.84")
+            }
         }
     }
 }
